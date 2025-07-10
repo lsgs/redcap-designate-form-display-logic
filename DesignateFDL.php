662c861ff191ebec218c37a100a3585d333f935f
Luke <lukestevens@hotmail.com>
2025-07-10 11:02:58 +1000

    Ready for test
    Scan passed, ready for deploy to redcap-test

--- DesignateFDL.php
+++ DesignateFDL.php
@@ -18,8 +18,7 @@ class DesignateFDL extends AbstractExternalModule
         if (!defined('PAGE') || PAGE!=='Design/designate_forms.php') return;
         global $Proj;
         if (!$Proj->project['repeatforms']) return; // module does nothing in non-longitudinal projects
-        // if (!\FormDisplayLogic::FormDisplayLogicEnabled($project_id)) return; // do nothing if no FDL
-
+        
         $event_unique_names = \REDCap::getEventNames(true, false);
 
         $fdl_conditions = \FormDisplayLogic::getFormDisplayLogicTableValues($project_id);
@@ -55,17 +54,32 @@ class DesignateFDL extends AbstractExternalModule
             }
         }
 
+        ?>
+        <script type="text/javascript">disable_instrument_table = true; // Designate Form Display Logic: initialisation required for displayFormDisplayLogicPopup()</script><?php
+        loadJS('DesignForms.js');
         $this->initializeJavascriptModuleObject();
         ?>
+        <style type="text/css">
+            #FormDisplayLogicSetupDialog div.data, #deleteAll { display:none !important; }
+        </style>
         <script type="text/javascript">
             // Designate Form Display Logic
             $(function(){
+                lang.designate_forms_13 = '<?=\RCView::tt_js('designate_forms_13')?>';
+                langErrorColon = '<?=\RCView::tt_js('global_01').\RCView::tt_js('colon')?>';
+                form_missing = '<?=\RCView::tt_js('design_988') ?>';
+                logic_missing = '<?=\RCView::tt_js('design_989') ?>';
+                duplicate_warning = '<?=\RCView::tt_js('design_971') ?>';
+                confirm_msg = '<?=\RCView::tt_js('design_972') ?>';
+                delete_conditions = function() { return false; }
+
                 var module = <?=$this->getJavascriptModuleObjectName()?>;
                 module.tt_add('fdl','<?=\RCView::tt_js('design_985')?>');
                 module.tt_add('view','<?=\RCView::tt_js('global_84')?>');
                 module.event_unique_names = <?=\json_encode_rc($event_unique_names)?>;
                 module.arm_events_forms = <?=\json_encode_rc($this->arm_events_forms)?>;
-                module.form_event_popover = '<a tabindex="0" class="" role="button" data-bs-toggle="popover" data-bs-placement="bottom"></a>'
+                module.event_form_conditions = [];
+                module.form_event_tooltip = '<a tabindex="0" class="DesignateFDL_view ml-1 fs10" role="button" data-bs-toggle="tooltip" data-bs-placement="bottom"><i class="fas fa-eye-slash"></i></a>'
                 module.getTickFormEvent = function(img) {
                     let id = $(img).attr('id'); // e.g. 'img--screening_form--3210
                     let td = $(img).parents('td:first');
@@ -99,35 +113,63 @@ class DesignateFDL extends AbstractExternalModule
                     });
                     return conditions;
                 };
-                module.popoverContent = function(el) {
-                    let tick = $(el).find('img:first');
+                module.insertFDLButton = function() {
+                    let tick = $(this);
+                    let event_name = module.getTickProperty(tick, 'event_name');
+                    let form_name = module.getTickProperty(tick, 'form_name');
+                    let conditions = module.getConditions(event_name, form_name);
+                    
+                    let col = (conditions.length) ? '#444' : '#ccc';
+
+                    let btn = $(module.form_event_tooltip);
+                    btn.on('click', conditions, module.launchFDL)
+                        .css('color', col)
+                        .insertAfter(this);
+                };
+                module.tooltipContent = function(el) {
+                    let tick = $(el).siblings('img:first');
                     let event_name = module.getTickProperty(tick, 'event_name');
                     let form_name = module.getTickProperty(tick, 'form_name');
                     
                     let conditions = module.getConditions(event_name, form_name);
                     let conditionText = '<b>'+conditions.length+'</b> condition'+((conditions.length===1)?'':'s');
 
-                    let content = '<b>'+module.getTickProperty(tick, 'event_label')+'</b>';
+                    let content = '<i class="fas fa-eye-slash"></i> '+module.tt('fdl'); // Form Display Logic
+                    content += '<br>'+module.getTickProperty(tick, 'event_label');
                     content += '<br>'+module.getTickProperty(tick, 'form_label');
                     content += '<br>'+conditionText;
                     return content;
                 }
-                module.init = function() {
-                    console.log(module.arm_events_forms);
-                    $('#event_grid_table img').wrap(function(){
-                        // wrap each form/event tick in a <a> with popover
-                        return module.form_event_popover;
+                module.launchFDL = function(evt) {
+                    $(".tooltip").tooltip("hide");
+                    module.event_form_conditions = evt.data;
+                    displayFormDisplayLogicPopup();
+                };
+                module.fdlDialogOpen = function() {
+                    // console.log(module.event_form_conditions);
+                    $('#FormDisplayLogicSetupDialog div.data').hide();
+                    $('button#deleteAll').hide();
+                    // hide all of the control conditions that are not applicable to the current selected event/form
+                    $('input[id^=control_id]').each(function(){
+                        let controlVal = $(this).val();
+                        if (!module.event_form_conditions.includes(controlVal)) {
+                            $(this).closest('div.repeater-divs').hide();
+                        }
                     });
-                    const popoverTriggerList = $('a[data-bs-toggle="popover"]');
-                    const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl, {
-                        trigger: 'focus',
+                };
+                module.init = function() {
+                    //console.log(module.arm_events_forms);
+                    $('#event_grid_table img').each(module.insertFDLButton);
+                    const tooltipTriggerList = $('a[data-bs-toggle="tooltip"]');
+                    const tooltipList = [...tooltipTriggerList].map(triggerEl => new bootstrap.Tooltip(triggerEl, {
                         html: true,
                         sanitize: false,
-                        title: '<i class="fas fa-eye-slash"></i> '+module.tt('fdl'), // Form Display Logic
-                        content: module.popoverContent(popoverTriggerEl)
+                        title: module.tooltipContent(triggerEl)
                     }));
-                    $('a.DesignateFDL_view').on('click',function(){
-                        console.log(this);
+                    $('body').on('dialogopen', function(event){
+                        if(event.target.id=='FormDisplayLogicSetupDialog') {
+                            module.fdlDialogOpen();
+                        }
                     });
                 };
 
